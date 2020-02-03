5f3db63f0049207d2439766a1bf48b32ef864fea
lkighy <1003027913@qq.com>
2020-02-03 19:24:52 +0800

    定义枚举

---
+++ Rust 程序设计语言学习笔记/枚举和模式匹配/match 控制流运算符.rs
@@ -0,0 +1,18 @@
+// 例如硬币
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+// 一直值一个分支
+// fn value_in_cents(coin: Coin) {
+//     match coin {
+//         Coin::Penny => 1,
+//         Coin::Nickel => 5,
+//         Coin::Dime => 10,
+//         Coin::Quarter => 25,
+//     }
+// }
+
--- Rust 程序设计语言学习笔记/枚举和模式匹配/定义枚举.rs
+++ Rust 程序设计语言学习笔记/枚举和模式匹配/定义枚举.rs
@@ -1,32 +1,93 @@
-// enum IpAddKind {
-//     v4,
-//     v6,
+//  定义枚举
+// 关键字 enum
+// 例如处理ip, IP
+enum IpAddrKind {
+    V4,
+    v6,
+}
+
+// 实例化
+let four = IpAddrKind::v4;
+let six = IpAddrKind::v6;
+
+// 这样可以定义一个函数来获取任何的 IpAddrKind
+
+fn route(ip_type: IpAddrKind) {}
+
+// 调用函数
+route(four);
+route(six);
+
+// 存储值: 使用结构体
+
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
+
+let home = IpAddr {
+    kind: IpAddrKind::v4,
+    address: String::from("127.0.0.1"),
+};
+
+let loopback = IpAddr {
+    kind: IpAddrKind::v6,
+    address: String::from("::1")
+};
+
+// 存储值
+
+// enum IpAddr {
+//     V4(String),
+//     V6(String),
 // }
 
-// struct IpAddr {
-//     Kind: IpAddKind,
-//     address: String,
-// }
-// fn main() {
-//     let four = IpAddKind::v4;
-//     let six = IpAddKind::v6;
+// let home = IpAddr::V4(String::from("127.0.0.1"));
 
-//     route(IpAddKind::v4);
-//     route(IpAddKind::v6);
+// let loopback = IpAddr::v6(String::from("::1"));
 
-//     let home = IpAddr {
-//         Kind: IpAddKind::v4,
-//         address: String::from("127.0.0.1"),
-//     }
-//     let loopback = IpAddr = {
-//         Kind: IpAddKind::v6,
-//         address: String::from("::1"),
-//     }
+// 存储值: 与元组使用
+
+// enum IpAddr {
+//     V4(u8, u8, u8, u8),
+//     V6(String),
 // }
 
-// fn route(ip_type: IpAddKind) {}
+
+// let home = IpAddr::V4(127, 0, 0, 1);
+
+// let loopback = IpAddr::v6(String::from("::1"));
+
+// 存储值: 存储 struct
+
+
+struct Ipv4Addr {}
+
+struct Ipv6Addr {}
 
 enum IpAddr {
-    V4(String),
-    V6(String),
+    v4(Ipv4Addr),
+    v6(Ipv6Addr),
+}
+
+// 内嵌多种类型, 例如下面的例子
+
+enum Message {
+    Quit,
+    Move {x: i32, y: i32},
+    Write!(String),
+    ChangeColor(i32, i32, i32),
+}
+
+// Quit 没有关联任何数据。
+// Move 包含一个匿名结构体。
+// Write 包含单独一个 String。
+// ChangeColor 包含三个 i32。
+
+// 结构体和枚举的另一个相似点: 可以使用 impl 来为结构定义方法那样在枚举上定义方法.
+
+impl Message {
+    fn call(&self) {
+        // 在这里定义方法体
+    }
 }
